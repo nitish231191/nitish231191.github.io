14402a3aa39e809f26aea9a177683c06df98b8f6
nitish231191 <nitishchandra2391@gmail.com>
2016-10-21 17:37:54 -0500

    HCC Commit

---
+++ TechSupport-master/Tipue drop 5.0.2/tipuedrop/tipuedrop_content.js
@@ -0,0 +1,5 @@
+var tipuedrop = {"pages": [
+     {"title": "Tipue Search", "thumb": "img/search.png", "text": "a site search engine jQuery plugin", "url": "http://www.tipue.com/search"},
+     {"title": "Tipue drop", "thumb": "img/drop.png", "text": "a search suggestion box jQuery plugin", "url": "http://www.tipue.com/drop"},
+     {"title": "Tipr", "thumb": "img/tipr.png", "text": "a small and simple tooltip jQuery plugin", "url": "http://www.tipue.com/tipr"}
+]};
